f669b9bca6f1dde096059fe2ff151c2f194cfc37
gibbsy81 <33105735+gibbsy81@users.noreply.github.com>
2017-10-26 09:38:55 +1100

    Fixed miss typed Write-Host Command

--- Reaper.ps1
+++ Reaper.ps1
@@ -6,94 +6,95 @@ Write-Host -NoNewLine 'Press any key to Debloat';
 $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
 Clear-Host
 #3D Builder: 
-Wirte-Host 'Removing 3D Builder'
+Write-Host 'Removing 3D Builder'
 Get-AppxPackage *3dbuilder* | Remove-AppxPackage
 Clear-Host
 $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
 #Alarms and Clock: 
-Wirte-Host 'Removing Alarms and Clock'
+Write-Host 'Removing Alarms and Clock'
 Get-AppxPackage *windowsalarms* | Remove-AppxPackage
 Clear-Host
 #Calculator: 
-Wirte-Host 'Removing Alarms and Clock'
+Write-Host 'Removing Alarms and Clock'
 Get-AppxPackage *windowscalculator* | Remove-AppxPackage
+Clear-Host
 #Calendar and Mail: 
-Wirte-Host 'Calender and Mail'
+Write-Host 'Calender and Mail'
 Get-AppxPackage *windowscommunicationsapps* | Remove-AppxPackage
 Clear-Host
 #Camera: 
-Wirte-Host 'Removing Camera'
+Write-Host 'Removing Camera'
 Get-AppxPackage *windowscamera* | Remove-AppxPackage
 Clear-Host
 #Get Office: 
-Wirte-Host 'Removing Get Office'
+Write-Host 'Removing Get Office'
 Get-AppxPackage *officehub* | Remove-AppxPackage
 Clear-Host
 #Get Skype: 
-Wirte-Host 'Removing Get Skype'
+Write-Host 'Removing Get Skype'
 Get-AppxPackage *skypeapp* | Remove-AppxPackage
 Clear-Host
 #Get Started: 
-Wirte-Host 'Removing Get Started'
+Write-Host 'Removing Get Started'
 Get-AppxPackage *getstarted* | Remove-AppxPackage
 Clear-Host
 #Groove Music: 
-Wirte-Host 'Removing Groove Music'
+Write-Host 'Removing Groove Music'
 Get-AppxPackage *zunemusic* | Remove-AppxPackage
 Clear-Host
 #Maps: 
-Wirte-Host 'Removing Maps'
+Write-Host 'Removing Maps'
 Get-AppxPackage *windowsmaps* | Remove-AppxPackage
 Clear-Host
 #Microsoft Solitaire Collection: 
-Wirte-Host 'Removing Microsoft Solitaire Collection'
+Write-Host 'Removing Microsoft Solitaire Collection'
 Get-AppxPackage *solitairecollection* | Remove-AppxPackage
 Clear-Host
 #Money: 
-Wirte-Host 'Removing Money'
+Write-Host 'Removing Money'
 Get-AppxPackage *bingfinance* | Remove-AppxPackage
 Clear-Host
 #Movies & TV: 
-Wirte-Host 'Removing Movies & TV'
+Write-Host 'Removing Movies & TV'
 Get-AppxPackage *zunevideo* | Remove-AppxPackage
 Clear-Host
 #News: 
-Wirte-Host 'Removing News'
+Write-Host 'Removing News'
 Get-AppxPackage *bingnews* | Remove-AppxPackage
 Clear-Host
 #OneNote: 
-Wirte-Host 'Removing OneNote'
+Write-Host 'Removing OneNote'
 Get-AppxPackage *onenote* | Remove-AppxPackage
 Clear-Host
 #People: 
-Wirte-Host 'Removing People'
+Write-Host 'Removing People'
 Get-AppxPackage *people* | Remove-AppxPackage
 Clear-Host
 #Phone Companion: 
-Wirte-Host 'Removing Phone Companion'
+Write-Host 'Removing Phone Companion'
 Get-AppxPackage *windowsphone* | Remove-AppxPackage
 Clear-Host
 #Photos: 
-Wirte-Host 'Removing Photos'
+Write-Host 'Removing Photos'
 Get-AppxPackage *photos* | Remove-AppxPackage
 Clear-Host
 #Store: 
-Wirte-Host 'Removing Store'
+Write-Host 'Removing Store'
 Get-AppxPackage *windowsstore* | Remove-AppxPackage
 Clear-Host
 #Sports: 
-Wirte-Host 'Removing Sports'
+Write-Host 'Removing Sports'
 Get-AppxPackage *bingsports* | Remove-AppxPackage
 Clear-Host
 #Voice Recorder: 
-Wirte-Host 'Removing Voice Recorder'
+Write-Host 'Removing Voice Recorder'
 Get-AppxPackage *soundrecorder* | Remove-AppxPackage
 Clear-Host
 #Weather: 
-Wirte-Host 'Removing Weather'
+Write-Host 'Removing Weather'
 Get-AppxPackage *bingweather* | Remove-AppxPackage
 Clear-Host
 #Xbox: 
-Wirte-Host 'Removing Xbox'
+Write-Host 'Removing Xbox'
 Get-AppxPackage *xboxapp* | Remove-AppxPackage
 Clear-Host
