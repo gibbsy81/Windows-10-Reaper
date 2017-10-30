f8b9a61bda8dbf65108ee1b80132e9c60eacbbdc
gibbsy81 <33105735+gibbsy81@users.noreply.github.com>
2017-10-30 19:54:28 +1100

    Final Commit for V2

--- ReaperV2/ReaperV2.ps1
+++ ReaperV2/ReaperV2.ps1
@@ -50,99 +50,102 @@ function InstallKeyConsumer
 #Uninstall Apps
 function Uninstall
 {
-Write-Host 'Working'
+    Write-Host 'Working'
 
-#3D Builder: 
-Write-Host 'Removing 3D Builder'
-Get-AppxPackage *3dbuilder* | Remove-AppxPackage
+    #3D Builder: 
+    Write-Host 'Removing 3D Builder'
+    Get-AppxPackage *3dbuilder* | Remove-AppxPackage
 
-#Alarms and Clock: 
-Write-Host 'Removing Alarms and Clock'
-Get-AppxPackage *windowsalarms* | Remove-AppxPackage
+    #Alarms and Clock: 
+    Write-Host 'Removing Alarms and Clock'
+    Get-AppxPackage *windowsalarms* | Remove-AppxPackage
 
-#Calculator: 
-Write-Host 'Removing Alarms and Clock'
-Get-AppxPackage *windowscalculator* | Remove-AppxPackage
+    #Calculator: 
+    Write-Host 'Removing Alarms and Clock'
+    Get-AppxPackage *windowscalculator* | Remove-AppxPackage
 
-#Calendar and Mail: 
-Write-Host 'Removing Calender and Mail'
-Get-AppxPackage *windowscommunicationsapps* | Remove-AppxPackage
+    #Calendar and Mail: 
+    Write-Host 'Removing Calender and Mail'
+    Get-AppxPackage *windowscommunicationsapps* | Remove-AppxPackage
 
-#Camera: 
-Write-Host 'Removing Camera'
-Get-AppxPackage *windowscamera* | Remove-AppxPackage
+    #Camera: 
+    Write-Host 'Removing Camera'
+    Get-AppxPackage *windowscamera* | Remove-AppxPackage
 
-#Get Office: 
-Write-Host 'Removing Get Office'
-Get-AppxPackage *officehub* | Remove-AppxPackage
+    #Get Office: 
+    Write-Host 'Removing Get Office'
+    Get-AppxPackage *officehub* | Remove-AppxPackage
 
-#Get Skype: 
-Write-Host 'Removing Get Skype'
-Get-AppxPackage *skypeapp* | Remove-AppxPackage
+    #Get Skype: 
+    Write-Host 'Removing Get Skype'
+    Get-AppxPackage *skypeapp* | Remove-AppxPackage
 
-#Get Started: 
-Write-Host 'Removing Get Started'
-Get-AppxPackage *getstarted* | Remove-AppxPackage
+    #Get Started: 
+    Write-Host 'Removing Get Started'
+    Get-AppxPackage *getstarted* | Remove-AppxPackage
 
-#Groove Music: 
-Write-Host 'Removing Groove Music'
-Get-AppxPackage *zunemusic* | Remove-AppxPackage
+    #Groove Music: 
+    Write-Host 'Removing Groove Music'
+    Get-AppxPackage *zunemusic* | Remove-AppxPackage
 
-#Maps: 
-Write-Host 'Removing Maps'
-Get-AppxPackage *windowsmaps* | Remove-AppxPackage
+    #Maps: 
+    Write-Host 'Removing Maps'
+    Get-AppxPackage *windowsmaps* | Remove-AppxPackage
 
-#Microsoft Solitaire Collection: 
-Write-Host 'Removing Microsoft Solitaire Collection'
-Get-AppxPackage *solitairecollection* | Remove-AppxPackage
+    #Microsoft Solitaire Collection: 
+    Write-Host 'Removing Microsoft Solitaire Collection'
+    Get-AppxPackage *solitairecollection* | Remove-AppxPackage
 
-#Money: 
-Write-Host 'Removing Money'
-Get-AppxPackage *bingfinance* | Remove-AppxPackage
+    #Money: 
+    Write-Host 'Removing Money'
+    Get-AppxPackage *bingfinance* | Remove-AppxPackage
 
-#Movies & TV: 
-Write-Host 'Removing Movies & TV'
-Get-AppxPackage *zunevideo* | Remove-AppxPackage
+    #Movies & TV: 
+    Write-Host 'Removing Movies & TV'
+    Get-AppxPackage *zunevideo* | Remove-AppxPackage
 
-#News: 
-Write-Host 'Removing News'
-Get-AppxPackage *bingnews* | Remove-AppxPackage
+    #News: 
+    Write-Host 'Removing News'
+    Get-AppxPackage *bingnews* | Remove-AppxPackage
 
-#OneNote: 
-Write-Host 'Removing OneNote'
-Get-AppxPackage *onenote* | Remove-AppxPackage
+    #OneNote: 
+    Write-Host 'Removing OneNote'
+    Get-AppxPackage *onenote* | Remove-AppxPackage
 
-#People: 
-Write-Host 'Removing People'
-Get-AppxPackage *people* | Remove-AppxPackage
+    #People: 
+    Write-Host 'Removing People'
+    Get-AppxPackage *people* | Remove-AppxPackage
 
-#Phone Companion: 
-Write-Host 'Removing Phone Companion'
-Get-AppxPackage *windowsphone* | Remove-AppxPackage
+    #Phone Companion: 
+    Write-Host 'Removing Phone Companion'
+    Get-AppxPackage *windowsphone* | Remove-AppxPackage
 
-#Photos: 
-Write-Host 'Removing Photos'
-Get-AppxPackage *photos* | Remove-AppxPackage
+    #Photos: 
+    Write-Host 'Removing Photos'
+    Get-AppxPackage *photos* | Remove-AppxPackage
 
-#Store: 
-Write-Host 'Removing Store'
-Get-AppxPackage *windowsstore* | Remove-AppxPackage
+    #Store: 
+    Write-Host 'Removing Store'
+    Get-AppxPackage *windowsstore* | Remove-AppxPackage
 
-#Sports: 
-Write-Host 'Removing Sports'
-Get-AppxPackage *bingsports* | Remove-AppxPackage
+    #Sports: 
+    Write-Host 'Removing Sports'
+    Get-AppxPackage *bingsports* | Remove-AppxPackage
 
-#Voice Recorder: 
-Write-Host 'Removing Voice Recorder'
-Get-AppxPackage *soundrecorder* | Remove-AppxPackage
+    #Voice Recorder: 
+    Write-Host 'Removing Voice Recorder'
+    Get-AppxPackage *soundrecorder* | Remove-AppxPackage
 
-#Weather: 
-Write-Host 'Removing Weather'
-Get-AppxPackage *bingweather* | Remove-AppxPackage
+    #Weather: 
+    Write-Host 'Removing Weather'
+    Get-AppxPackage *bingweather* | Remove-AppxPackage
 
-#Xbox: 
-Write-Host 'Removing Xbox'
-Get-AppxPackage *xboxapp* | Remove-AppxPackage
+    #Xbox: 
+    Write-Host 'Removing Xbox'
+    Get-AppxPackage *xboxapp* | Remove-AppxPackage
+
+    Write-Host 'Finished'
+
 }
 
 function End
